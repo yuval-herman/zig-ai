09cd94b93b3fc7c4134c8257a5afd2bf5759471d
Yuval Herman <yuvalherman99@gmail.com>
2024-09-12 11:03:36 +0300

    prepare MLP for multithreading

--- src/mlp.zig
+++ src/mlp.zig
@@ -63,92 +63,112 @@ pub fn MLP(NETWORK_STRUCTURE: []const u32) type {
             }
 
             for (&mlp.biases, &mlp.bias_grads) |*bias, *grad| {
-                bias.* = random.float(f64);
+                bias.* = random.float(f64) / 100;
                 grad.* = 0;
             }
             for (&mlp.weights, &mlp.weights_grads) |*weight, *grad| {
-                weight.* = random.float(f64) / 100;
+                weight.* = random.float(f64) / 1000;
                 grad.* = 0;
             }
 
             return mlp;
         }
 
-        pub fn forward(self: *ThisType, input: []const f64) []f64 {
+        // `self: *const ThisType` found to be faster then `self: ThisType` after profiling
+        fn inner_forward(self: *const ThisType, layers_output: [][]f64, activated_layers_output: [][]f64, input: []const f64) []f64 {
             assert(input.len == NETWORK_STRUCTURE[0]);
             var bias_index: usize = 0;
             var weight_index: usize = 0;
-            self.activated_layers_output[0] = @constCast(input);
-            self.layers_output[0] = @constCast(input);
+            activated_layers_output[0] = @constCast(input);
+            layers_output[0] = @constCast(input);
 
             inline for (1..NETWORK_STRUCTURE.len) |layer_index| {
                 for (0..NETWORK_STRUCTURE[layer_index]) |out_index| {
-                    const out = &self.layers_output[layer_index][out_index];
+                    const out = &layers_output[layer_index][out_index];
                     out.* = self.biases[bias_index];
                     bias_index += 1;
 
                     const vec_size = NETWORK_STRUCTURE[layer_index - 1];
                     const vec_type = @Vector(vec_size, f64);
 
-                    const inputs: vec_type = self.activated_layers_output[layer_index - 1][0..vec_size].*;
+                    const inputs: vec_type = activated_layers_output[layer_index - 1][0..vec_size].*;
                     const weights: vec_type = self.weights[weight_index..][0..vec_size].*;
                     weight_index += vec_size;
                     out.* += @reduce(.Add, inputs * weights);
 
-                    self.activated_layers_output[layer_index][out_index] = activation(out.*);
+                    activated_layers_output[layer_index][out_index] = activation(out.*);
                 }
             }
 
-            return self.activated_layers_output[self.activated_layers_output.len - 1];
+            return activated_layers_output[activated_layers_output.len - 1];
+        }
+
+        pub fn forward(self: *ThisType, input: []const f64) []f64 {
+            return self.inner_forward(&self.layers_output, &self.activated_layers_output, input);
         }
 
         pub fn backprop(self: *ThisType, batch: anytype) void {
+            self.extra_backprop(&self.bias_grads, &self.weights_grads, &self.layers_output, &self.activated_layers_output, batch);
+            self.applyGrads();
+        }
+
+        pub fn extra_backprop(
+            self: *const ThisType,
+            bias_grads: []f64,
+            weights_grads: []f64,
+            layers_output: [][]f64,
+            activated_layers_output: [][]f64,
+            node_derivatives: [][]f64,
+            batch: anytype,
+        ) void {
             while (batch.next()) |data_point| {
-                _ = self.forward(data_point.input);
+                _ = self.inner_forward(layers_output, activated_layers_output, data_point.input);
 
                 var bias_index: usize = bias_amount;
                 var weight_index: usize = weights_amount;
 
-                self.activated_layers_output[0] = @constCast(data_point.input);
+                activated_layers_output[0] = @constCast(data_point.input);
+
                 for (
-                    self.activated_layers_output[NETWORK_STRUCTURE.len - 1],
-                    self.layers_output[NETWORK_STRUCTURE.len - 1],
-                    self.node_derivatives[NETWORK_STRUCTURE.len - 1],
+                    activated_layers_output[NETWORK_STRUCTURE.len - 1],
+                    layers_output[NETWORK_STRUCTURE.len - 1],
+                    node_derivatives[NETWORK_STRUCTURE.len - 1],
                     data_point.output,
                 ) |a_output, output, *node_derivative, expected_value| {
                     node_derivative.* = costDerivative(a_output, expected_value) * activationDerivative(output);
                 }
                 comptime var layer_index = NETWORK_STRUCTURE.len - 1;
                 inline while (layer_index > 0) : (layer_index -= 1) {
-                    @memset(self.node_derivatives[layer_index - 1], 0);
+                    @memset(node_derivatives[layer_index - 1], 0);
                     var out_index = NETWORK_STRUCTURE[layer_index];
                     while (out_index > 0) {
                         out_index -= 1;
                         bias_index -= 1;
-                        self.bias_grads[bias_index] += self.node_derivatives[layer_index][out_index];
+                        bias_grads[bias_index] += node_derivatives[layer_index][out_index];
 
                         const vec_size = NETWORK_STRUCTURE[layer_index - 1];
                         const vec_type = @Vector(vec_size, f64);
 
-                        var grads_vec: vec_type = self.weights_grads[weight_index - vec_size ..][0..vec_size].*;
-                        const activated_layers_output: vec_type = self.activated_layers_output[layer_index - 1][0..vec_size].*;
-                        const node_derivative: vec_type = @splat(self.node_derivatives[layer_index][out_index]);
+                        var grads_vec: vec_type = weights_grads[weight_index - vec_size ..][0..vec_size].*;
+                        const activated_layers_output_vec: vec_type = activated_layers_output[layer_index - 1][0..vec_size].*;
+                        const node_derivative: vec_type = @splat(node_derivatives[layer_index][out_index]);
 
-                        grads_vec += activated_layers_output * node_derivative;
-                        self.weights_grads[weight_index - vec_size ..][0..vec_size].* = grads_vec;
+                        grads_vec += activated_layers_output_vec * node_derivative;
+                        weights_grads[weight_index - vec_size ..][0..vec_size].* = grads_vec;
 
-                        var prev_node_derivatives: vec_type = self.node_derivatives[layer_index - 1][0..vec_size].*;
+                        var prev_node_derivatives: vec_type = node_derivatives[layer_index - 1][0..vec_size].*;
                         const weights: vec_type = self.weights[weight_index - vec_size ..][0..vec_size].*;
                         prev_node_derivatives += weights * node_derivative;
-                        self.node_derivatives[layer_index - 1][0..vec_size].* = prev_node_derivatives;
+                        node_derivatives[layer_index - 1][0..vec_size].* = prev_node_derivatives;
                         weight_index -= vec_size;
                     }
-                    for (self.node_derivatives[layer_index - 1], self.layers_output[layer_index - 1]) |*node_derivative, z| {
+                    for (node_derivatives[layer_index - 1], layers_output[layer_index - 1]) |*node_derivative, z| {
                         node_derivative.* *= activationDerivative(z);
                     }
                 }
             }
-
+        }
+        pub fn applyGrads(self: *ThisType) void {
             for (&self.weights, &self.weights_grads) |*weight, *grad| {
                 weight.* -= grad.* * self.learn_rate;
                 grad.* = 0;
